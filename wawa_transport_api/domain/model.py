8ab8cfcb228ed5d6e0d4782c1b484cfb5d311836
pr0f37 <93840714+pr0f37@users.noreply.github.com>
2022-07-21 21:46:10 +0200

    Closest bus stops

--- wawa_transport_api/domain/model.py
+++ wawa_transport_api/domain/model.py
@@ -3,10 +3,19 @@ from dataclasses import dataclass
 
 
 @dataclass
+class Coordinates:
+    """Class representing geographical coordinates"""
+
+    lat: float
+    lon: float
+
+
+@dataclass
 class BusStop:
     """Class representing the buss or tram stop"""
 
     id: str
     number: str
-    lat: float
-    lon: float
+    coords: Coordinates
+    name: str
+    direction: str
